d57fea4a124e085374645fd10cf131dfd347f3a4
Milena Figueredo <milena.silva@zup.com.br>
2019-08-02 16:31:04 -0300

    Adicionar classe de generos literarios

---
+++ src/main/java/com/br/zup/matchbook/models/GenerosLiterarios.java
@@ -0,0 +1,52 @@
+package com.br.zup.matchbook.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class GenerosLiterarios implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull(message = "o nome é um campo obrigatorio")
+	private String nome;
+
+	private Usuario usuario;
+	
+	public GenerosLiterarios() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+}
