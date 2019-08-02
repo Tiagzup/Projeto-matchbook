9a73a9db4aaa2dc3299a04ad70910724b463e5b2
Tiago Souza <tiago.souza@zup.com.br>
2019-08-02 15:02:45 -0300

    Criar projeto matchbook

---
+++ src/main/java/com/br/zup/matchbook/ProjetoMatchbookApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.matchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMatchbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMatchbookApplication.class, args);
+	}
+
+}
