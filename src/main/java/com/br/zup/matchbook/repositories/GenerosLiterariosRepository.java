32144aa0f9ae69535a0da38c3a1e81c4e1b16b97
Bruna <bruna.lima@zup.com.br>
2019-08-02 17:05:35 -0300

    Correção de Idioma e ortografia.

--- src/main/java/com/br/zup/matchbook/repositories/GenerosLiterariosRepository.java
+++ src/main/java/com/br/zup/matchbook/repositories/GenerosLiterariosRepository.java
@@ -3,9 +3,9 @@ package com.br.zup.matchbook.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.br.zup.matchbook.models.GenerosLiterarios;
+import com.br.zup.matchbook.models.LiteraryGenre;
 
 @Repository
-public interface GenerosLiterariosRepository extends CrudRepository<GenerosLiterarios, Integer> {
+public interface GenerosLiterariosRepository extends CrudRepository<LiteraryGenre, Integer> {
 
 }
