335757750129ba697e5282bf8bee7bdcedeada05
Tiago Souza <tiago.souza@zup.com.br>
2019-08-05 15:32:21 -0300

    Crir TDD loginService

--- src/main/java/com/br/zup/matchbook/repositories/LoginRepository.java
+++ src/main/java/com/br/zup/matchbook/repositories/LoginRepository.java
@@ -1,5 +1,7 @@
 package com.br.zup.matchbook.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.br.zup.matchbook.models.Login;
 
 @Repository
 public interface LoginRepository extends CrudRepository<Login, Integer>{
+
+
+
+	Optional<Login> findByNicknameAndPassword (String nickname, String password);
 
 }
---
+++ src/main/java/com/br/zup/matchbook/services/LoginService.java
@@ -0,0 +1,30 @@
+package com.br.zup.matchbook.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.zup.matchbook.models.Login;
+import com.br.zup.matchbook.models.User;
+import com.br.zup.matchbook.repositories.LoginRepository;
+
+@Service
+public class LoginService {
+	@Autowired
+	private LoginRepository loginRepository;
+	
+	
+	public String registerLogin (User user, Login login) {
+		login.setUser(user);
+		loginRepository.save(login);
+		return "Login cadastrado";
+	}
+	public void updateLogin (Login login) {
+		loginRepository.save(login);
+	}
+	public void deleteLogin (Login login) {
+		loginRepository.delete(login);
+	}
+	public Login findByNickAndPass (Login login) {
+		return loginRepository.findByNicknameAndPassword(login.getNickname(), login.getPassword()).get();
+	}
+}
